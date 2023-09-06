ecf23a020f95a24b0c88ab2eb6ec5c53c005fe71
DebanjanChatterjeeMERNSTACK <debanjanthechatterjee@gmail.com>
2023-09-06 09:08:40 -0700

    CHANGES

--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./slider.css"
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 import image from "../component/logo/bodyspray.jpg";
 import image1 from "../component/logo/iphone.jpg";
 import image2 from "../component/logo/watch.jpg";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 
 
@@ -31,6 +35,33 @@ const Sliderimg = () => {
 
     return (
         <>
+            <div className='sliderimg1'>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 1000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><img src={image} className='image' /></SwiperSlide>
+                    <SwiperSlide><img src={image1} className='image' /></SwiperSlide>
+                    <SwiperSlide><img src={image2} className='image' /></SwiperSlide>
+                   
+                </Swiper>
+            </div>
+
+
+
+
+
+
             {/* <div className='sliderimg1'>
                 <Slider {...settings}>
                     <div>
@@ -44,9 +75,12 @@ const Sliderimg = () => {
                     </div>
                 </Slider>
             </div> */}
-            <div className='sliderimg1'>
+
+
+
+            {/* <div className='sliderimg1'>
                 <img src={image1} className='image' />
-            </div>
+            </div> */}
 
         </>
     );
